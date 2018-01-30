e523ce23034f74e246d796759af687f31df830cf
XueGi0 <15535955420@163.com>
2018-01-30 15:30:53 +0800

    目录更改

--- src/container/Personal/Personal.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default class Personal extends React.Component {
-  render(){
-    return (
-    <div>
-    Personal
-    </div>)
-  }
-}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
+import Home from "./containers/Home/Home";
+
+
+
+ReactDOM.render(<Home/>, window.root);
